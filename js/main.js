23a30d38c58964d23ab91a39432755558817b40c
Ved17-11-09 <90040144+Ved17-11-09@users.noreply.github.com>
2024-03-04 18:00:27 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,101 @@
+let play = document.getElementById("audio1");
+let pause = document.getElementById("pause1");
+let audio = new Audio('music/Badass.mp3');
+
+play.addEventListener('click', ()=>{
+    audio.play();
+})
+pause.addEventListener('click', ()=>{
+    audio.pause();
+})
+
+
+
+let play2 = document.getElementById("audio2");
+let pause2 = document.getElementById("pause2");
+let audio2 = new Audio('music/Lokiverse_2.0.mp3');
+
+play2.addEventListener('click', ()=>{
+    audio2.play();
+})
+pause2.addEventListener('click', ()=>{
+    audio2.pause();
+})
+
+
+
+let play3 = document.getElementById("audio3");
+let pause3 = document.getElementById("pause3");
+let audio3 = new Audio('music/Na_Ready.mp4');
+
+play3.addEventListener('click', ()=>{
+    audio3.play();
+})
+pause3.addEventListener('click', ()=>{
+    audio3.pause();
+})
+
+
+
+let play4 = document.getElementById("audio4");
+let pause4 = document.getElementById("pause4");
+let audio4 = new Audio('music/im_scared.mp3');
+
+play4.addEventListener('click', ()=>{
+    audio4.play();
+})
+pause4.addEventListener('click', ()=>{
+    audio4.pause();
+})
+
+
+
+let play5 = document.getElementById("audio5");
+let pause5 = document.getElementById("pause5");
+let audio5= new Audio('music/vy.mp3');
+
+play5.addEventListener('click', ()=>{
+    audio5.play();
+})
+pause5.addEventListener('click', ()=>{
+    audio5.pause();
+})
+
+
+
+let play6 = document.getElementById("audio6");
+let pause6 = document.getElementById("pause6");
+let audio6 = new Audio('music/ad.mp3');
+
+play6.addEventListener('click', ()=>{
+    audio6.play();
+})
+pause6.addEventListener('click', ()=>{
+    audio6.pause();
+})
+
+
+
+let play7 = document.getElementById("audio7");
+let pause7 = document.getElementById("pause7");
+let audio7 = new Audio('music/hd.mp3');
+
+play7.addEventListener('click', ()=>{
+    audio7.play();
+})
+pause7.addEventListener('click', ()=>{
+    audio7.pause();
+})
+
+
+
+let play8 = document.getElementById("audio8");
+let pause8 = document.getElementById("pause8");
+let audio8 = new Audio('music/op.mp3');
+
+play8.addEventListener('click', ()=>{
+    audio8.play();
+})
+pause8.addEventListener('click', ()=>{
+    audio8.pause();
+})
